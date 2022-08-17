52ffe1c87afbed668af27294f4a47c17966adc13
Morzh777 <103659109+Morzh777@users.noreply.github.com>
2022-08-17 17:10:28 +0300

    Добавляет адаптивную сетку

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,14 +1,14 @@
-const navigationSelector = document.querySelector('.main-nav');
-const navigationButtonSelector = document.querySelector('.main-nav__toggle');
+const navSelector = document.querySelector('.main-nav');
+const navButtonSelector = document.querySelector('.main-nav__toggle');
 
-const onButtonSelectorClick = () =>{
-  if (navigationSelector.classList.contains('main-nav--close')) {
-    navigationSelector.classList.remove('main-nav--close');
-    navigationSelector.classList.add('main-nav--open');
+const onNavButtonSelectorClick = () =>{
+  if (navSelector.classList.contains('main-nav--close')) {
+    navSelector.classList.remove('main-nav--close');
+    navSelector.classList.add('main-nav--open');
   } else {
-    navigationSelector.classList.add('main-nav--close');
-    navigationSelector.classList.remove('main-nav--open');
+    navSelector.classList.add('main-nav--close');
+    navSelector.classList.remove('main-nav--open');
   }
 };
 
-navigationButtonSelector.addEventListener ('click',onButtonSelectorClick);
+navButtonSelector.addEventListener ('click',onNavButtonSelectorClick);
